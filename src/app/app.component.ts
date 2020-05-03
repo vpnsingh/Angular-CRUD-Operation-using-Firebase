f3f8c3f0ac0f6601dbe6e7db6861d3788271f3f5
vpnsingh <vs181715@gmail.com>
2020-05-03 21:13:37 +0530

    changes in directory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myapp';
+  title = 'CRUDFireBase';
+
+  constructor(private router: Router) { }
+
+  btnClick = function () {
+    this.router.navigateByUrl('/login');
+};
 }
